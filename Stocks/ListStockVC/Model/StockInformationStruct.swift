6592896fb2c6eebbd0eb634d3f2a2106a65ada78
Danila <dobrotdanila@gmail.com>
2020-08-31 12:06:59 +0300

    Исправил баг с временем торгов и изменением цены

--- Stocks/ListStockVC/Model/StockInformationStruct.swift
+++ Stocks/ListStockVC/Model/StockInformationStruct.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2020 Dobrotolyubov Danila. All rights reserved.
 //
 
+
+//      Я ДОПУСТИЛ ОШИБКУ, ВЫЧИСЛЯЯ ЗНАЧЕНИЕ change.
+//      ЕГО НАДО БЫЛО БРАТЬ ИХ json. ЗНАЧЕНИЯ openTime, closeTime ВООБЩЕ НЕ НУЖНЫ!
+//      А ЗНАЧЕНИЕ openPrice МОЖЕТ ВЕРНУТЬСЯ КАК null. ИЗ-ЗА ЭТОГО ИЗНАЧАЛЬНАЯ ВЕРСИЯ
+//      ПРОГРАММЫ РАБОТАЕТ ТОЛЬКО КОГДА ЭТИ ПАРАМЕТРЫ НЕ null. Я ПОНЯЛ, ЧТО ЭТО ПОСЛЕ
+//      ЗАКРЫТИЯ ТОРГОВ В США.
+
+//      В ДАННОМ КЛАССЕ ВСЕ ИСПРАВЛЕННО И ГОТОВО К РАБОТЕ!
 import UIKit
 
 class StockInformation {
@@ -13,9 +21,9 @@ class StockInformation {
     let ticker:String
     let companyName:String
     let exchange:String
-    let openTime:Int
-    let closeTime:Int
-    let openPrice:Double
+//    let openTime:Int
+//    let closeTime:Int
+//    let openPrice:Double
     let latestPrice:Double
     let change: Double
     var image: UIImage?
@@ -25,19 +33,20 @@ class StockInformation {
         guard let ticker = json["symbol"] as? String,
         let companyName = json["companyName"] as? String,
         let exchange = json["primaryExchange"] as? String,
-        let openTime = json["openTime"] as? Int,
-        let closeTime = json["closeTime"] as? Int,
-        let openPrice = json["open"] as? Double,
+//        let openTime = json["openTime"] as? Int,
+//        let closeTime = json["closeTime"] as? Int,
+//        let openPrice = json["open"] as? Double,
+        let change = json["change"] as? Double,
         let latestPrice = json["latestPrice"] as? Double else { return nil }
         
         self.ticker = ticker
         self.companyName = companyName
         self.exchange = exchange
-        self.openTime = openTime
-        self.closeTime = closeTime
-        self.openPrice = openPrice
+//        self.openTime = openTime
+//        self.closeTime = closeTime
+//        self.openPrice = openPrice
         self.latestPrice = latestPrice
-        self.change = ((latestPrice - openPrice)*1000).rounded()/1000
+        self.change = change //((latestPrice - openPrice)*1000).rounded()/1000
     }
     
     init? (stock: Stock) {
@@ -51,10 +60,10 @@ class StockInformation {
         self.companyName = name
         self.ticker = ticker
         self.exchange = ""
-        self.openTime = 0
-        self.closeTime = 0
+//        self.openTime = 0
+//        self.closeTime = 0
         self.latestPrice = stock.price
         self.change = stock.changePrice
-        self.openPrice = 0
+//        self.openPrice = 0
     }
 }
